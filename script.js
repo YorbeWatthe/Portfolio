df05cebd57d5ebe39d838cf6833e7b9fe190cb69
YorbeWatthe <159446445+YorbeWatthe@users.noreply.github.com>
2024-05-15 09:31:44 +0200

    eerste commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector('.burger');
+const mobileNav = document.querySelector('.mobile-nav');
+
+burger.addEventListener('click', () => {
+  mobileNav.classList.toggle('active');
+});
